1fe2ffceb542608cec45a772c6865950181980f9
Kuba Stanczyk <kstanczyk7134@gmail.com>
2022-10-14 18:15:57 +0200

    prettier

--- client/src/AnimatedRoutes.js
+++ client/src/AnimatedRoutes.js
@@ -1,7 +1,4 @@
-import {
-    Routes,
-    Route,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MoviePage from "./components/MoviePage";
 import MyMovies from "./components/MyMovies";
 import Register from "./components/Register";
@@ -9,29 +6,38 @@ import Login from "./components/Login";
 import Movies from "./components/Movies";
 import { useLocation } from "react-router-dom";
 import ChangePassword from "./components/ChangePassword";
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from "framer-motion";
 
 const AnimatedRoutes = (props) => {
-
     const location = useLocation();
 
     return (
-        <AnimatePresence mode='wait'>
+        <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<MyMovies
-                    fav={props.fav}
-                    toggle={props.toggle}
-                    setToggle={props.setToggle}
-                />} />
-                <Route path="/search" element={<Movies
-                    results={props.results}
-                    fav={props.fav}
-                    toggle={props.toggle}
-                    setToggle={props.setToggle}
-                    results2={props.results2}
-                    handleSubmitMore={props.handleSubmitMore}
-                    loading={props.loading}
-                />} />
+                <Route
+                    path="/"
+                    element={
+                        <MyMovies
+                            fav={props.fav}
+                            toggle={props.toggle}
+                            setToggle={props.setToggle}
+                        />
+                    }
+                />
+                <Route
+                    path="/search"
+                    element={
+                        <Movies
+                            results={props.results}
+                            fav={props.fav}
+                            toggle={props.toggle}
+                            setToggle={props.setToggle}
+                            results2={props.results2}
+                            handleSubmitMore={props.handleSubmitMore}
+                            loading={props.loading}
+                        />
+                    }
+                />
                 <Route path="/moviepage/:imdbID" element={<MoviePage />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
@@ -39,6 +45,6 @@ const AnimatedRoutes = (props) => {
             </Routes>
         </AnimatePresence>
     );
-}
+};
 
 export default AnimatedRoutes;
--- server/index.js
+++ server/index.js
@@ -1,218 +1,247 @@
-const express = require("express")
-const cors = require("cors")
-const axios = require("axios")
-const rateLimit = require('express-rate-limit')
-const slowDown = require("express-slow-down")
-const mongoose = require("mongoose")
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const User = require('./model/user')
-const Count = require('./model/count')
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./model/user");
+const Count = require("./model/count");
+require("dotenv").config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const CONNECTION_URL = process.env.CONNECTION_URL
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 8000;
 
-mongoose.connect(CONNECTION_URL, {dbName: "movies"})
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message))
+mongoose
+    .connect(CONNECTION_URL, { dbName: "movies" })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    )
+    .catch((error) => console.log(error.message));
 
 const limiter = rateLimit({
     windowMs: 30 * 1000, // 30 seconds
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+});
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+app.use(limiter);
 
 const speedLimiter = slowDown({
     windowMs: 30 * 1000, // 15 minutes
     delayAfter: 20, // allow 100 requests per 15 minutes, then...
-    delayMs: 500 // begin adding 500ms of delay per request above 100:
+    delayMs: 500, // begin adding 500ms of delay per request above 100:
 });
 //  apply to all requests
 app.use(speedLimiter);
 
-app.get("/", (req,res) => {
-  res.json("WORKING")
-})
+app.get("/", (req, res) => {
+    res.json("WORKING");
+});
 
-app.get('/favourites', async (req, res) => {
-    const token = req.query.token
+app.get("/favourites", async (req, res) => {
+    const token = req.query.token;
     if (!token) {
-      return res.status(401).json({ err: "User token missing" })
+        return res.status(401).json({ err: "User token missing" });
     }
     try {
-      const user = jwt.verify(token, process.env.JWT_SECRET)
-      const _id = user.id
-      const document = await User.findOne({ _id }).lean()
-      const movies = document.movies
-      res.status(200).json(movies)
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const _id = user.id;
+        const document = await User.findOne({ _id }).lean();
+        const movies = document.movies;
+        res.status(200).json(movies);
     } catch (error) {
-      console.log(error)
-      res.status(401).json({ err: "You were loggod out." })
+        console.log(error);
+        res.status(401).json({ err: "You were loggod out." });
     }
-  })
+});
 
-  app.post('/favourites', async (req, res) => {
-    const movie = req.body.movie
-    const token = req.body.token
+app.post("/favourites", async (req, res) => {
+    const movie = req.body.movie;
+    const token = req.body.token;
     try {
-      const user = jwt.verify(token, process.env.JWT_SECRET)
-      const _id = user.id
-      await User.updateOne(
-        { _id },
-        {
-          $push: {
-            movies: movie
-          }
-        })
-      res.json({ status: 'ok' })
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const _id = user.id;
+        await User.updateOne(
+            { _id },
+            {
+                $push: {
+                    movies: movie,
+                },
+            }
+        );
+        res.json({ status: "ok" });
     } catch (error) {
-      console.log(error)
-      res.json({ status: 'error', error: "mess" })
+        console.log(error);
+        res.json({ status: "error", error: "mess" });
     }
-  })
+});
 
-  app.delete('/favourites', async (req, res) => {
-    const movie = req.body.imdbID
-    const token = req.body.token
+app.delete("/favourites", async (req, res) => {
+    const movie = req.body.imdbID;
+    const token = req.body.token;
     try {
-      const user = jwt.verify(token, process.env.JWT_SECRET)
-      const _id = user.id
-      await User.updateOne(
-        { _id },
-        {
-          $pull: {
-            movies: { imdbID: movie }
-          }
-        })
-      res.json({ status: 'ok' })
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const _id = user.id;
+        await User.updateOne(
+            { _id },
+            {
+                $pull: {
+                    movies: { imdbID: movie },
+                },
+            }
+        );
+        res.json({ status: "ok" });
     } catch (error) {
-      console.log(error)
-      res.json({ status: 'error', error: "mess" })
+        console.log(error);
+        res.json({ status: "error", error: "mess" });
     }
-  })
+});
 
-  app.post("/register", async (req, res) => {
-    const { login: username, password: plainTextPassword } = req.body
-    if (!username || typeof username !== 'string') {
-      return res.json({ status: 'error', error: 'invalid username' })
+app.post("/register", async (req, res) => {
+    const { login: username, password: plainTextPassword } = req.body;
+    if (!username || typeof username !== "string") {
+        return res.json({ status: "error", error: "invalid username" });
     }
-    const password = await bcrypt.hash(plainTextPassword, 10)
+    const password = await bcrypt.hash(plainTextPassword, 10);
     try {
-      const response = await User.create({
-        username,
-        password
-      })
-      console.log('user created', response)
+        const response = await User.create({
+            username,
+            password,
+        });
+        console.log("user created", response);
     } catch (error) {
-      if (error.code === 11000) {
-        return res.status(409).json(`Username: ${username} is already used!`)
-      }
-      throw error
+        if (error.code === 11000) {
+            return res
+                .status(409)
+                .json(`Username: ${username} is already used!`);
+        }
+        throw error;
     }
-    res.status(201).json(`${username}, your account  has been created. You can sign in now`)
-  })
+    res.status(201).json(
+        `${username}, your account  has been created. You can sign in now`
+    );
+});
 
-  app.post("/login", async (req, res) => {
-    const { login: username, password } = req.body
-    const user = await User.findOne({ username }).lean()
+app.post("/login", async (req, res) => {
+    const { login: username, password } = req.body;
+    const user = await User.findOne({ username }).lean();
     if (!user) {
-      return res.json({ status: 'error', data: "Invalid username/password" })
+        return res.json({ status: "error", data: "Invalid username/password" });
     }
     if (await bcrypt.compare(password, user.password)) {
-      const token = jwt.sign({
-        id: user._id,
-        username: user.username,
-        exp: Math.floor((Date.now() / 1000) + 60 * 60 * 3)
-      }, process.env.JWT_SECRET)
-      return res.json({ status: 'ok', data: token, username: user.username })
+        const token = jwt.sign(
+            {
+                id: user._id,
+                username: user.username,
+                exp: Math.floor(Date.now() / 1000 + 60 * 60 * 3),
+            },
+            process.env.JWT_SECRET
+        );
+        return res.json({ status: "ok", data: token, username: user.username });
     }
-    res.json({ status: "error", data: "Invalid username/password" })
-  })
+    res.json({ status: "error", data: "Invalid username/password" });
+});
 
-  app.post('/changepassword', async (req, res) => {
-    const { token, password: newPassword } = req.body
+app.post("/changepassword", async (req, res) => {
+    const { token, password: newPassword } = req.body;
     if (!token) {
-      return res.status(401).json({ err: "User token missing" })
+        return res.status(401).json({ err: "User token missing" });
     }
     try {
-      const user = jwt.verify(token, process.env.JWT_SECRET)
-      const _id = user.id
-      const password = await bcrypt.hash(newPassword, 10)
-      await User.updateOne({ _id }, {
-        $set: { password: password }
-      })
-      res.json({ status: 'ok' })
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const _id = user.id;
+        const password = await bcrypt.hash(newPassword, 10);
+        await User.updateOne(
+            { _id },
+            {
+                $set: { password: password },
+            }
+        );
+        res.json({ status: "ok" });
     } catch (error) {
-      console.log(error)
-      res.json({ status: 'error', error: "mess" })
+        console.log(error);
+        res.json({ status: "error", error: "mess" });
     }
-  })
+});
 
-  async function requestCounter(req, res, next) {
+async function requestCounter(req, res, next) {
     try {
-        const reqCount = await Count.findOne( {_id: "63490d7a647128da4175191d"} )
-        if( reqCount.count < 1000 ){
-            reqCount.count++
-            await reqCount.save()
-            next()
+        const reqCount = await Count.findOne({
+            _id: "63490d7a647128da4175191d",
+        });
+        if (reqCount.count < 1000) {
+            reqCount.count++;
+            await reqCount.save();
+            next();
         } else {
-            res.status(403).json("Daily request limit reached")
+            res.status(403).json("Daily request limit reached");
         }
     } catch (e) {
-        console.log(e.message)
+        console.log(e.message);
     }
-  }
-  app.use(requestCounter)
+}
+app.use(requestCounter);
 
-  let cachedData;
-  let cacheTime;
+let cachedData;
+let cacheTime;
 
-  app.get("/search", (req, res) => {
+app.get("/search", (req, res) => {
     if (cacheTime && cacheTime > Date.now() - 1000) {
-      cachedData.wait = true;
-      return res.json(cachedData)
+        cachedData.wait = true;
+        return res.json(cachedData);
     }
     const options = {
-      method: 'GET',
-      url: 'https://movie-database-alternative.p.rapidapi.com/',
-      params: { s: req.query.s, r: 'json', page: req.query.page, type: req.query.type, year: req.query.year },
-      headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
-      }
+        method: "GET",
+        url: "https://movie-database-alternative.p.rapidapi.com/",
+        params: {
+            s: req.query.s,
+            r: "json",
+            page: req.query.page,
+            type: req.query.type,
+            year: req.query.year,
+        },
+        headers: {
+            "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+            "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
+        },
     };
-    axios.request(options).then(function (response) {
-      cachedData = response.data;
-      cacheTime = Date.now();
-      response.data.wait = false
-      response.data.cacheTime = cacheTime;
-      res.json(response.data);
-    }).catch(function (error) {
-      console.error(error);
-    });
-  })
+    axios
+        .request(options)
+        .then(function (response) {
+            cachedData = response.data;
+            cacheTime = Date.now();
+            response.data.wait = false;
+            response.data.cacheTime = cacheTime;
+            res.json(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+});
 
-  app.get("/movie", (req, res) => {
+app.get("/movie", (req, res) => {
     const options = {
-      method: 'GET',
-      url: 'https://movie-database-alternative.p.rapidapi.com/',
-      params: { r: 'json', i: req.query.id },
-      headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
-      }
+        method: "GET",
+        url: "https://movie-database-alternative.p.rapidapi.com/",
+        params: { r: "json", i: req.query.id },
+        headers: {
+            "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+            "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
+        },
     };
-    axios.request(options).then(function (response) {
-      res.json(response.data)
-    }).catch(function (error) {
-      console.error(error);
-    });
-  })
+    axios
+        .request(options)
+        .then(function (response) {
+            res.json(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+});
