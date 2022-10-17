7f6698fc75b7cbf6b48d006b1b4f624aad4fe056
kuba7134 <kstanczyk7134@gmail.com>
2022-10-17 18:35:23 +0200

    ??

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { BsFillCameraReelsFill } from "react-icons/bs";
 import { AiFillHome, AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
-<<<<<<< HEAD
 const Navbar = props => {
   const [toggle, setToggle] = useState(true);
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -204,89 +203,5 @@ const Navbar = props => {
     </nav>
   );
 };
-=======
-const Navbar = (props) => {
-    const [toggle, setToggle] = useState(true)
-    let navigate = useNavigate();
-
-    useEffect(() => {
-    }, [toggle])
-
-    return (
-        <nav>
-            <Link style={{ textDecoration: 'none', color: "black" }} to={`/`}>
-                <motion.h1 initial={{ fontSize: 0, opacity: 0 }} animate={{ fontSize: "1.5em", opacity: 1, transition: { duration: .3 } }}>My moviess</motion.h1>
-            </Link>
-            <motion.div
-                className="rightSide"
-                initial={{ scale: .1, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1, transition: { duration: .3 } }}
-            >
-                {localStorage.user ?
-                    <>
-                        {localStorage.user &&
-                            <>
-                                User: <Link style={{ textDecoration: 'none', color: "black", fontWeight: "650" }} to="/changepassword">{localStorage.user}</Link>
-                            </>}
-                        <button className="navbar-button" onClick={() => {
-                            navigate("/login", { replace: true })
-                            localStorage.clear()
-                            props.setFav("")
-                            setToggle(!toggle)
-                        }
-                        }>Logout</button>
-                    </>
-                    :
-                    <button className="navbar-button" onClick={() => {
-                        navigate("/login", { replace: true })
-                    }
-                    }>Login</button>
-                }
-
-            </motion.div>
-            <form onSubmit={props.handleSubmit}>
-                    <div id="title" className="fancy-input">
-                        <input
-                            type="text"
-                            placeholder=" "
-                            required
-                            className="form-input"
-                            id="search"
-                            value={props.title}
-                            onChange={(e) => props.setTitle(e.target.value)}
-                        />
-                        <label htmlFor="search" className="form-label">Title</label>
-                    </div>
-                    <div className="fancy-input">
-                        <select
-                            name="Type"
-                            className="select-type"
-                            value={props.type}
-                            onChange={(e) => props.setType(e.target.value)}
-                        >
-                            <option value="">All</option>
-                            <option value="movie">Movie</option>
-                            <option value="series">Series</option>
-                        </select>
-                    </div>
-                    <div className="fancy-input">
-                        <input
-                            type="number"
-                            min={1900}
-                            max={2030}
-                            id="year"
-                            placeholder=" "
-                            className="form-input"
-                            value={props.year}
-                            onChange={(e) => props.setYear(e.target.value)}
-                        />
-                        <label htmlFor="year" className="form-label">Year</label>
-                    </div>
-                    <button onClick={() => navigate("/search", { replace: true })}>Search</button>
-                </form>
-        </nav>
-    );
-}
->>>>>>> fa99b5dc4060f251afec193f4507f3df624ee8eb
 
 export default Navbar;
--- client/src/components/SingleMovie.js
+++ client/src/components/SingleMovie.js
@@ -2,7 +2,6 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { motion, AnimatePresence } from "framer-motion";
 
-<<<<<<< HEAD
 const Movie = props => {
   let navigate = useNavigate();
   let data = { movie: props.movie, token: localStorage.token };
@@ -51,53 +50,6 @@ const Movie = props => {
       transition: { duration: 0.3 },
     },
   };
-=======
-const Movie = (props) => {
-
-    let navigate = useNavigate();
-    let data = { movie: props.movie, token: localStorage.token }
-    let data2 = { imdbID: props.movie.imdbID, token: localStorage.token }
-
-    const handleClick = () => {
-        axios.post('https://my-movies7.herokuapp.com/favourites', data)
-            .then(function (response) {
-                console.log(response);
-            })
-            .then(() => {
-                props.setToggle(!props.toggle)
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    const handleClick2 = () => {
-        axios.delete('https://my-movies7.herokuapp.com/favourites', { data: data2 })
-            .then(function (response) {
-                console.log(response);
-            })
-            .then(() => {
-                props.setToggle(!props.toggle)
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    const buttonVariants = {
-        initial: {
-            scale: 1
-        },
-        hidden: {
-            scale: [1, 1.3, 0],
-            transition: { duration: .3, type: "spring", stiffness: 500}
-        },
-        visible: {
-            scale: [0, 1.3, 1],
-            transition: { duration: .3}
-        }
-    }
->>>>>>> fa99b5dc4060f251afec193f4507f3df624ee8eb
 
   return (
     <motion.div
@@ -124,7 +76,6 @@ const Movie = (props) => {
           href={"https://www.imdb.com/title/" + props.movie.imdbID}
           target="_blank"
         >
-<<<<<<< HEAD
           <img
             src="https://i.pinimg.com/originals/e7/17/2e/e7172e01718bc174bc237b5971659548.png"
             alt="imdb-logo"
@@ -168,59 +119,5 @@ const Movie = (props) => {
     </motion.div>
   );
 };
-=======
-            <motion.img
-                onClick={() => navigate(`/moviepage/${props.movie.imdbID}`)}
-                className="poster"
-                src={props.movie.Poster}
-                alt={props.movie.Title + " - poster"}
-                initial={{ scale: .1, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-            />
-            <p>{props.movie.Title}</p>
-            <div className="movie-info">
-                <span className="type">Type: {props.movie.Type}</span>
-                <span className="year">{props.movie.Year}</span>
-            </div>
-            <div className="hidden-icon">
-                <a href={"https://www.imdb.com/title/" + props.movie.imdbID} target="_blank">
-                    <img src="https://i.pinimg.com/originals/e7/17/2e/e7172e01718bc174bc237b5971659548.png" alt="imdb-logo" />
-                </a>
-                <AnimatePresence mode="wait">
-                    {
-                        props.delSign === "login" ?
-                            <img
-                                className="adddel"
-                                onClick={() => navigate("/login")}
-                                src="https://www.clipartmax.com/png/full/43-433845_ambox-emblem-plus-green-plus-symbol.png"
-                                alt="add button" />
-                            : props.delSign ?
-                                <motion.img
-                                    variants={buttonVariants}
-                                    initial="initial"
-                                    animate="visible"
-                                    exit="hidden"
-                                    key="minus"
-                                    className="adddel"
-                                    onClick={handleClick2}
-                                    src="https://i.pinimg.com/originals/d0/17/47/d01747c4285afa4e7a6e8656c9cd60cb.png"
-                                    alt="add button" />
-                                :
-                                <motion.img
-                                    variants={buttonVariants}
-                                    initial="initial"
-                                    animate="visible"
-                                    exit="hidden"
-                                    key="plus"
-                                    className="adddel" onClick={handleClick}
-                                    src="https://www.clipartmax.com/png/full/43-433845_ambox-emblem-plus-green-plus-symbol.png"
-                                    alt="add button" />
-                    }
-                </AnimatePresence>
-            </div>
-        </motion.div>
-    );
-}
->>>>>>> fa99b5dc4060f251afec193f4507f3df624ee8eb
 
 export default Movie;
